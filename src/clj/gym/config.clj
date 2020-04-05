2c064e2eefcbc5de388baf0cc7766d9b4c3583f3
zrcni <zrcni9@gmail.com>
2020-04-05 21:48:54 +0300

    get config from project.clj?

--- src/clj/gym/config.clj
+++ src/clj/gym/config.clj
@@ -1,6 +1,7 @@
-(ns gym.config)
+(ns gym.config
+  (:require [environ.core :refer [env]]))
 
-(def frontend-url (System/getenv "FRONTEND_URL"))
-(def jdbc-database-url (System/getenv "JDBC_DATABASE_URL"))
-(def port (System/getenv "PORT"))
-(def host-url (System/getenv "HOST_URL"))
+(def frontend-url (:frontend-url env))
+(def jdbc-database-url (:jdbc-database-url env))
+(def port (:port env))
+(def host-url (:host-url env))
