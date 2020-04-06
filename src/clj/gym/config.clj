e21b5a0c39ccd30bf838d44a21e231df31fdb82b
zrcni <zrcni9@gmail.com>
2020-04-06 08:02:30 +0300

    fixed string/split usage

--- src/clj/gym/config.clj
+++ src/clj/gym/config.clj
@@ -2,7 +2,7 @@
   (:require [environ.core :refer [env]]
             [clojure.string :refer [split]]))
 
-(def frontend-urls (split (:frontend-urls env) ","))
+(def frontend-urls (split (:frontend-urls env) #","))
 (def jdbc-database-url (:jdbc-database-url env))
 (def port (:port env))
 (def host-url (:host-url env))
