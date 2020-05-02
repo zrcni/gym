8a04bde8944c5730562e36f3cd9a43b9e1bdc9fe
zrcni <zrcni9@gmail.com>
2020-05-02 19:48:17 +0300

    use js prefix for initiating Auth0Client class instance

--- src/cljs/gym/auth.cljs
+++ src/cljs/gym/auth.cljs
@@ -13,14 +13,13 @@
 
 ;; https://auth0.github.io/auth0-spa-js/interfaces/auth0clientoptions.html
 (defn create-auth0-client []
-  (prn "auth0spa: " auth0spa)
-  (new (.-Auth0Client auth0spa) (clj->js
-                                 {:domain "samulir.eu.auth0.com"
-                                  :client_id cfg/auth0-client-id
-                                  :display "popup"
-                                  :useRefreshTokens true
-                                  :cacheLocation "localstorage"
-                                  :redirect_uri (str (-> js/window .-location .-origin) "/auth0_callback")})))
+  (new js/auth0spa.Auth0Client (clj->js
+                                {:domain "samulir.eu.auth0.com"
+                                 :client_id cfg/auth0-client-id
+                                 :display "popup"
+                                 :useRefreshTokens true
+                                 :cacheLocation "localstorage"
+                                 :redirect_uri (str (-> js/window .-location .-origin) "/auth0_callback")})))
 
 (defn reg-auth0-cofx [client]
   (reg-cofx
