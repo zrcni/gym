5a0eb26df734ee579067cd2506829c7f075e6d4e
zrcni <zrcni9@gmail.com>
2020-05-03 21:26:26 +0300

    added title to header (link to root page) removed username and moved avatar to the right, next to logout button

--- src/cljs/gym/views.cljs
+++ src/cljs/gym/views.cljs
@@ -49,10 +49,10 @@
       [:<>
        [:header {:id "header" :class "navbar navbar-expand navbar-dark flex-column flex-md-row bd-navbar"}
         [:div.header-left
+         [:a.header-title {:href "/"} "Exercise tracker"]]
+        [:div.header-right
          (when (:avatar_url user)
            [:img.header-user-avatar {:src (:avatar_url user) :alt "user-logo"}])
-         [:span.header-username (:username user)]]
-        [:div.header-right
          (when user [:button.logout-button {:on-click #(dispatch [:logout])} "Logout"])]]
        [:main {:id "content"}
         children]])))
