1ba5ea4a3181dae73741aed10e69b5d3e8a42e78
zrcni <zrcni9@gmail.com>
2022-03-08 22:27:10 +0200

    added support for excluding tags in the rest of the analytics queries

--- src/clj/gym/backend/analytics/query_defs.clj
+++ src/clj/gym/backend/analytics/query_defs.clj
@@ -11,62 +11,90 @@
       [value]
       nil)))
 
+
 (def workout-duration-by-tag
   {:build (fn [{:keys [user-id exclude]}]
-            (let [exclude (search-param->vector exclude)]
+            (let [exclude (search-param->vector exclude)
+                  where [:= :public.workouts.user_id user-id]
+                  where (if (empty? exclude)
+                          where
+                          [:and where
+                           [:not-in :tag exclude]])]
+
               (-> (h/select [:tags.tag #_as :tag] [[:sum :public.workouts.duration] #_as :duration])
                   (h/from :public.workouts)
                   (h/left-join [:public.workout_tags :tags] [:= :public.workouts.workout_id :tags.workout_id])
-                  (h/where (let [where [:= :public.workouts.user_id user-id]]
-                             (if (empty? exclude)
-                               where
-                               [:and where
-                                [:not-in :tag exclude]])))
+                  (h/where where)
                   (h/group-by :tags.tag)
                   (h/order-by [:tags.tag :asc])
                   (sql/format))))
    :params {:req [:user-id]
             :opt [:exclude]}})
 
+
 (def workout-duration-this-week
-  {:build (fn [{:keys [user-id]}]
-            (-> (h/select [[:sum :public.workouts.duration] #_as :duration])
-                (h/from :public.workouts)
-                (h/where [:and
-                          [:= :public.workouts.user_id user-id]
-                          [[:>= :public.workouts.date [:date_trunc :'week' [:now]]]]])
-                (sql/format)))
-   :params {:req [:user-id]}
+  {:build (fn [{:keys [user-id exclude]}]
+            (let [exclude (search-param->vector exclude)
+                  where [:and
+                         [:= :public.workouts.user_id user-id]
+                         [[:>= :public.workouts.date [:date_trunc :'week' [:now]]]]]
+                  where (if (empty? exclude)
+                          where
+                          (update where 1 conj [:not-in :tag exclude]))]
+
+              (-> (h/select [[:sum :public.workouts.duration] #_as :duration])
+                  (h/from :public.workouts)
+                  (h/where where)
+                  (sql/format))))
+   :params {:req [:user-id]
+            :opt [:exclude]}
    :resolve
    (fn [res]
      {:duration (or (-> res first :duration) 0)})})
+
 
 (def workout-duration-this-month
-  {:build (fn [{:keys [user-id]}]
-            (-> (h/select [[:sum :public.workouts.duration] #_as :duration])
-                (h/from :public.workouts)
-                (h/where [:and
-                          [:= :public.workouts.user_id user-id]
-                          [[:>= :public.workouts.date [:date_trunc :'month' [:now]]]]])
-                (sql/format)))
-   :params {:req [:user-id]}
+  {:build (fn [{:keys [user-id exclude]}]
+            (let [exclude (search-param->vector exclude)
+                  where [:and
+                         [:= :public.workouts.user_id user-id]
+                         [[:>= :public.workouts.date [:date_trunc :'month' [:now]]]]]
+                  where (if (empty? exclude)
+                          where
+                          (update where 1 conj [:not-in :tag exclude]))]
+
+              (-> (h/select [[:sum :public.workouts.duration] #_as :duration])
+                  (h/from :public.workouts)
+                  (h/where where)
+                  (sql/format))))
+   :params {:req [:user-id]
+            :opt [:exclude]}
    :resolve
    (fn [res]
      {:duration (or (-> res first :duration) 0)})})
 
+
 (def workouts-by-day-of-week
-  {:build (fn [{:keys [user-id]}]
-            (-> (h/select [[:cast [:extract [:raw "isodow FROM public.workouts.date"]] #_as :integer] #_as :date]
-                          [:tags.tag #_as :tag]
-                          [:%count.* #_as :count])
-                (h/from :public.workouts)
-                (h/left-join [:public.workout_tags :tags] [:= :public.workouts.workout_id :tags.workout_id])
-                (h/where [:= :public.workouts.user_id user-id])
-                (h/group-by [:cast [:extract [:raw "isodow FROM public.workouts.date"]] #_as :integer] :tags.tag)
-                (h/order-by [[:cast [:extract [:raw "isodow FROM public.workouts.date"]] #_as :integer] :asc]
-                            [:tags.tag :asc])
-                (sql/format)))
-   :params {:req [:user-id]}
+  {:build (fn [{:keys [user-id exclude]}]
+            (let [exclude (search-param->vector exclude)
+                  where [:= :public.workouts.user_id user-id]
+                  where (if (empty? exclude)
+                          where
+                          [:and where
+                           [:not-in :tag exclude]])]
+
+              (-> (h/select [[:cast [:extract [:raw "isodow FROM public.workouts.date"]] #_as :integer] #_as :date]
+                            [:tags.tag #_as :tag]
+                            [:%count.* #_as :count])
+                  (h/from :public.workouts)
+                  (h/left-join [:public.workout_tags :tags] [:= :public.workouts.workout_id :tags.workout_id])
+                  (h/where where)
+                  (h/group-by [:cast [:extract [:raw "isodow FROM public.workouts.date"]] #_as :integer] :tags.tag)
+                  (h/order-by [[:cast [:extract [:raw "isodow FROM public.workouts.date"]] #_as :integer] :asc]
+                              [:tags.tag :asc])
+                  (sql/format))))
+   :params {:req [:user-id]
+            :opt [:exclude]}
    :resolve
    (fn [res]
      {:all-tags (reduce #(conj %1 (:tag %2)) #{} res)
@@ -78,19 +106,28 @@
                                    {:date date}
                                    maps))))})})
 
+
 (def workouts-by-month-of-year
-  {:build (fn [{:keys [user-id]}]
-            (-> (h/select [[:cast [:extract [:raw "month FROM public.workouts.date"]] #_as :integer] #_as :date]
-                          [:tags.tag #_as :tag]
-                          [:%count.* #_as :count])
-                (h/from :public.workouts)
-                (h/left-join [:public.workout_tags :tags] [:= :public.workouts.workout_id :tags.workout_id])
-                (h/where [:= :public.workouts.user_id user-id])
-                (h/group-by [:cast [:extract [:raw "month FROM public.workouts.date"]] #_as :integer] :tags.tag)
-                (h/order-by [[:cast [:extract [:raw "month FROM public.workouts.date"]] #_as :integer] :asc]
-                            [:tags.tag :asc])
-                (sql/format)))
-   :params {:req [:user-id]}
+  {:build (fn [{:keys [user-id exclude]}]
+            (let [exclude (search-param->vector exclude)
+                  where [:= :public.workouts.user_id user-id]
+                  where (if (empty? exclude)
+                          where
+                          [:and where
+                           [:not-in :tag exclude]])]
+
+              (-> (h/select [[:cast [:extract [:raw "month FROM public.workouts.date"]] #_as :integer] #_as :date]
+                            [:tags.tag #_as :tag]
+                            [:%count.* #_as :count])
+                  (h/from :public.workouts)
+                  (h/left-join [:public.workout_tags :tags] [:= :public.workouts.workout_id :tags.workout_id])
+                  (h/where where)
+                  (h/group-by [:cast [:extract [:raw "month FROM public.workouts.date"]] #_as :integer] :tags.tag)
+                  (h/order-by [[:cast [:extract [:raw "month FROM public.workouts.date"]] #_as :integer] :asc]
+                              [:tags.tag :asc])
+                  (sql/format))))
+   :params {:req [:user-id]
+            :opt [:exclude]}
    :resolve
    (fn [res]
      {:all-tags (reduce #(conj %1 (:tag %2)) #{} res)
@@ -102,6 +139,7 @@
                                    {:date date}
                                    maps)))
                     (sort-by :date))})})
+
 
 (def query-defs
   {:workout-duration-this-week workout-duration-this-week
