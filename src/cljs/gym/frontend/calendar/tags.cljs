e3ef40a29540a548b8c7db61ee613dab4548a3e3
zrcni <zrcni9@gmail.com>
2022-12-15 09:02:47 +0200

    deduplicate suggested tags

--- src/cljs/gym/frontend/calendar/tags.cljs
+++ src/cljs/gym/frontend/calendar/tags.cljs
@@ -1,6 +1,8 @@
 (ns gym.frontend.calendar.tags)
 
-(def ^:private max-suggested-tags 3)
+(def ^:private max-count 3)
 
 (defn consolidate-suggested-tags [prev curr]
-  (take max-suggested-tags (concat curr prev)))
+  (->> (concat curr prev)
+       (distinct)
+       (take max-count)))
